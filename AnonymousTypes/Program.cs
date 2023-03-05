3047e3d0172941cb6dbdae4d11cc700a03061192
KarolRogowski <karol.rogowski@gmail.com>
2023-03-05 16:55:02 +0100

    work work work

--- AnonymousTypes/Program.cs
+++ AnonymousTypes/Program.cs
@@ -1,31 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text.Json;
 using AnonymousTypes.Utils;
 
-var inst1 = new { id =1, Name = "Karol"};
-var inst2 = new { id =1, Name = "Karol"};
-Console.WriteLine(inst1.ToString());
-Console.WriteLine("inst1.Equals(inst2)");
-Console.WriteLine(inst1.Equals(inst2));
-Console.WriteLine("inst1 == inst2");
-Console.WriteLine(inst1 == inst2);
-
-
-var orders = Order.GetOrders();
-var orderSummaries = orders.Select(o => new
+// // AnonymousTypes START
+// var inst1 = new { id =1, Name = "Karol"};
+// var inst2 = new { id =1, Name = "Karol"};
+// Console.WriteLine(inst1.ToString());
+// Console.WriteLine("inst1.Equals(inst2)");
+// Console.WriteLine(inst1.Equals(inst2));
+// Console.WriteLine("inst1 == inst2");
+// Console.WriteLine(inst1 == inst2);
+//
+//
+// var orders = Order.GetOrders();
+// var orderSummaries = orders.Select(o => new
+// {
+//     o.Id,
+//     Items = o.Items?.Count,
+//     TotalValue = o.Items?.Sum(i => i.Price)
+// });
+//
+//
+// var inst3 = new { id = 1, value = 10 };
+// var inst4 = inst3 with { value = 12 };
+// Console.WriteLine(inst3.ToString());
+// Console.WriteLine(inst4.ToString());
+// // AnonymousTypes END
+// // Tuples START
+var order = Order.GetOrders().FirstOrDefault();
+if (order != null)
 {
-    o.Id,
-    Items = o.Items?.Count,
-    TotalValue = o.Items?.Sum(i => i.Price)
-});
+    var example1 = (OrderId: order.Id, OrderName: order.Name, ItemsCount: order.Items!.Count);
 
+    Console.WriteLine("example1");
+    Console.WriteLine(example1);
+    example1.ItemsCount++;
+    Console.WriteLine(example1);
+    example1.ItemsCount--;
 
-var inst3 = new { id = 1, value = 10 };
-var inst4 = inst3 with { value = 12 };
-Console.WriteLine(inst3.ToString());
-Console.WriteLine(inst4.ToString());
+    var example2 = (OrderId: order.Id,OrderName: order.Name,ItemsCount: order.Items!.Count);
+    Console.WriteLine("example1 == example2");
+    Console.WriteLine(example1 == example2);
 
+    
+    var jsonTP = JsonSerializer.Serialize(example1);
+    Console.WriteLine("Tuple serialized");
+    Console.WriteLine(jsonTP);
+    var exampleAT = new { OrderId = order.Id, OrderName = order.Name, ItemsCount = order.Items!.Count };
+    var jsonAT = JsonSerializer.Serialize(exampleAT);
+    Console.WriteLine("Anonymus serialized");
+    Console.WriteLine(jsonAT);
+    
+    
+    Console.WriteLine(TupleMethod(example1).ToOneString());
+}
+
+// Tuples END
 var i = 1;
+
+static ValueTuple<int,string, string> TupleMethod((int Input_OrderId, string Input_OrderName, int Input_ItemsCount) input)
+{
+    return (input.Input_OrderId, input.Input_OrderName, input.Input_ItemsCount.ToString());
+}
+
 
 
 
---
+++ AnonymousTypes/Utils/TupleExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace AnonymousTypes.Utils;
+
+public static class TupleExtensions
+{
+    public static string ToOneString(this ValueTuple<int,string, string> input)
+    {
+        return $"ID: {input.Item1.ToString()}, Name: {input.Item2}, Number of Items: {input.Item3}";
+    }
+}
